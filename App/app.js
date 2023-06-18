ecf7c487e4fb905b26bba77031d18d9f3797d312
Souvik Banerjee <souvikb019@gmail.com>
2023-06-18 21:00:22 +0530

    first commit

---
+++ App/app.js
@@ -0,0 +1,232 @@
+let canvas = document.querySelector("#tetris");
+let scoreboard = document.querySelector("h2");
+let ctx = canvas.getContext("2d");
+ctx.scale(30, 30);
+
+const SHAPES = [
+    [
+        [0, 1, 0, 0],
+        [0, 1, 0, 0],
+        [0, 1, 0, 0],
+        [0, 1, 0, 0]
+    ],
+    [
+        [0, 1, 0],
+        [0, 1, 0],
+        [1, 1, 0]
+    ],
+    [
+        [0, 1, 0],
+        [0, 1, 0],
+        [0, 1, 1]
+    ],
+    [
+        [1, 1, 0],
+        [0, 1, 1],
+        [0, 0, 0]
+    ],
+    [
+        [0, 1, 1],
+        [1, 1, 0],
+        [0, 0, 0]
+    ],
+    [
+        [1, 1, 1],
+        [0, 1, 0],
+        [0, 0, 0]
+    ],
+    [
+        [1, 1],
+        [1, 1],
+    ]
+]
+
+const COLORS = [
+    "#fff",
+    "#9b5fe0",
+    "#16a4d8",
+    "#60dbe8",
+    "#8bd346",
+    "#efdf48",
+    "#f9a52c",
+    "#d64e12"
+]
+
+const ROWS = 20;
+const COLS = 10;
+
+let grid = generateGrid();
+let PieceObj = null;
+let score = 0;
+
+setInterval(newGameState, 500);
+
+function newGameState() {
+    checkGrid();
+    if (!PieceObj) {
+        PieceObj = randomPieceObject();
+        renderPiece();
+    }
+    moveDown();
+}
+
+function checkGrid() {
+    let count = 0;
+    for (let i = 0; i < grid.length; i++) {
+        let allFilled = true;
+        for (let j = 0; j < grid[0].length; j++) {
+            if (grid[i][j] == 0) {
+                allFilled = false
+            }
+        }
+        if (allFilled) {
+            count++;
+            grid.splice(i, 1);
+            grid.unshift([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+        }
+    }
+    if (count == 1) {
+        score += 10;
+    } else if (count == 2) {
+        score += 30;
+    } else if (count == 3) {
+        score += 50;
+    } else if (count > 3) {
+        score += 100
+    }
+    scoreboard.innerHTML = "Score: " + score;
+}
+
+function generateGrid() {
+    let grid = [];
+    for (let i = 0; i < ROWS; i++) {
+        grid.push([]);
+        for (let j = 0; j < COLS; j++) {
+            grid[i].push(0)
+        }
+    }
+    return grid;
+}
+
+function randomPieceObject() {
+    let ran = Math.floor(Math.random() * 7);
+    let piece = SHAPES[ran];
+    let colorIndex = ran + 1;
+    let x = 4;
+    let y = 0;
+    return { piece, colorIndex, x, y }
+}
+
+function renderPiece() {
+    let piece = PieceObj.piece;
+    for (let i = 0; i < piece.length; i++) {
+        for (let j = 0; j < piece[i].length; j++) {
+            if (piece[i][j] == 1) {
+                ctx.fillStyle = COLORS[PieceObj.colorIndex];
+                ctx.fillRect(PieceObj.x + j, PieceObj.y + i, 1, 1);
+            }
+        }
+    }
+}
+
+function moveDown() {
+    if (!collision(PieceObj.x, PieceObj.y + 1))
+        PieceObj.y += 1;
+    else {
+        let piece = PieceObj.piece
+        for (let i = 0; i < piece.length; i++) {
+            for (let j = 0; j < piece[i].length; j++) {
+                if (piece[i][j] == 1) {
+                    let p = PieceObj.x + j;
+                    let q = PieceObj.y + i;
+                    grid[q][p] = PieceObj.colorIndex;
+                }
+            }
+        }
+        if (PieceObj.y == 0) {
+            alert("gamer over");
+            grid = generateGrid();
+            score = 0;
+        }
+        PieceObj = null;
+    }
+    renderGame();
+}
+
+function moveLeft() {
+    if (!collision(PieceObj.x - 1, PieceObj.y))
+        PieceObj.x -= 1;
+    renderGame();
+}
+
+function moveRight() {
+    if (!collision(PieceObj.x + 1, PieceObj.y))
+        PieceObj.x += 1;
+    renderGame();
+}
+
+function rotate() {
+    let rotatedPiece = [];
+    let piece = PieceObj.piece;
+    for (let i = 0; i < piece.length; i++) {
+        rotatedPiece.push([]);
+        for (let j = 0; j < piece[i].length; j++) {
+            rotatedPiece[i].push(0);
+        }
+    }
+    for (let i = 0; i < piece.length; i++) {
+        for (let j = 0; j < piece[i].length; j++) {
+            rotatedPiece[i][j] = piece[j][i]
+        }
+    }
+
+    for (let i = 0; i < rotatedPiece.length; i++) {
+        rotatedPiece[i] = rotatedPiece[i].reverse();
+    }
+    if (!collision(PieceObj.x, PieceObj.y, rotatedPiece))
+        PieceObj.piece = rotatedPiece
+    renderGame()
+}
+
+function collision(x, y, rotatedPiece) {
+    let piece = rotatedPiece || PieceObj.piece
+    for (let i = 0; i < piece.length; i++) {
+        for (let j = 0; j < piece[i].length; j++) {
+            if (piece[i][j] == 1) {
+                let p = x + j;
+                let q = y + i;
+                if (p >= 0 && p < COLS && q >= 0 && q < ROWS) {
+                    if (grid[q][p] > 0) {
+                        return true;
+                    }
+                } else {
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
+
+function renderGame() {
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[i].length; j++) {
+            ctx.fillStyle = COLORS[grid[i][j]];
+            ctx.fillRect(j, i, 1, 1)
+        }
+    }
+    renderPiece();
+}
+
+document.addEventListener("keydown", function(e) {
+    let key = e.key;
+    if (key == "ArrowDown") {
+        moveDown();
+    } else if (key == "ArrowLeft") {
+        moveLeft();
+    } else if (key == "ArrowRight") {
+        moveRight();
+    } else if (key == "ArrowUp") {
+        rotate();
+    }
+})
---
+++ README.md
@@ -0,0 +1,34 @@
+# Tetris_Game
+Tetris Game using JavaScript, HTML and CSS
+
+![Alt text](Assests/Capture.PNG)
+
+Play the Game in browser via ⏭️
+Copy the Following Link and Open in your Browser.
+
+```
+https://github.com/Souvik2376/Tetris-Game
+```
+
+    HOW TO PLAY ⏭️
+```  
+  ⬆️ Up-Arrow (Rotate)
+  ⬅️ Left-Arrow (Move Left)
+  ➡️ Right-Arrow (Move Right)
+  ⬇️ Down-Arrow (Increase Down Speed)
+```
+
+  Steps To Setup on Your Device 😀
+ 
+ ```
+  1. Clone the repository ©️
+        Open Terminal on a New Empty Folder.
+        copy the following command on the terminal and Enter ->  https://github.com/Souvik2376/Tetris-Game.git
+
+
+  2. Open The Created Folder via VsCode 🆚
+  
+  3. Play The Game by open with Live Server on LocalHost 🎭▶️⏯️
+
+  4. Up Arrow key (ROTATE) functionality uses the concept of Matrix Rotation.
+```
